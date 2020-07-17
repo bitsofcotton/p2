40e798d2c08c2726f083279c0256fb54198af09d
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2020-07-18 01:10:43 +0900

    Add files via upload

---
+++ magentap2.py
@@ -0,0 +1,37 @@
+import subprocess
+import sys
+
+tbl = [36, 38, 40, 41, 43, 45, 47]
+for u in range(1, 4):
+  for s in range(0, 7):
+    tbl.append(tbl[s] + 12 * u)
+rng = 20000
+
+p = subprocess.Popen(['./p'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+t = 0
+b = 0
+for line in sys.stdin:
+ for l in line:
+  if(l == '\n' or l == '.' or l == '-' or l == ' '):
+    continue
+  t += 1
+  b += int(l)
+  if(t % 6 != 0):
+    b *= 10
+    continue
+  # XXX: This only places note on corrected random way.
+  #      So this is not a music in certain definition.
+  mul = int(sys.argv[1])
+  p.stdin.write((str(b % (len(tbl) * mul)) + "\n").encode("utf-8"))
+  p.stdin.flush()
+  f = tbl[int((int(float(p.stdout.readline().decode("utf-8").split(",")[0]) / int(sys.argv[2])) % (len(tbl) * mul)) / mul)]
+  #f = tbl[b % len(tbl)]
+  if(36 + 12 * 2 <= f):
+    print("    { pitch: ", f, ", startTime: ", t / 12., ", endTime: ", t / 12. + .5, "},")
+  b = 0
+  if(rng < t):
+    break
+ if(rng < t):
+   break
+mid.save('rand_correct.mid')
+
