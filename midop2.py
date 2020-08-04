55eee2f675c98283e02502f3ac9af82aa9706a20
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2020-08-04 23:11:45 +0900

    Add files via upload

--- midop2.py
+++ midop2.py
@@ -3,17 +3,9 @@ from mido import Message, MidiFile, MidiTrack, MetaMessage
 import subprocess
 import sys
 
-"""
-tbl = [36, 38, 40, 41, 43, 45, 47]
-for u in range(1, 5):
-  for s in range(0, 7):
-    tbl.append(tbl[s] + 12 * u)
-"""
-
 tbl = []
 for u in range(0, 5 * 12):
   tbl.append(36 + u)
-rng = int(sys.argv[1]) * int(sys.argv[2])
 
 # Thanks to : https://qiita.com/tjsurume/items/75a96381fd57d5350971 via search engine
 mid   = MidiFile()
@@ -22,28 +14,25 @@ mid.tracks.append(track)
 track.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(100)))
 
 p = subprocess.Popen(['./p'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-t = s = bb = 0
-for t in range(0, rng * int(sys.argv[1])):
+flg = False
+for t in range(0, int(sys.argv[1])):
   # convert unpredictable to a little predictable ones.
   try:
-    b  = int(pow(1.5, float(sys.stdin.readline().split(",")[0].split(" ")[0]) / 2000.) * 100)
+    b = float(sys.stdin.readline().split(",")[0].split(" ")[0])
   except:
     continue
   # XXX: This only places note on corrected random way.
   #      So this is not a music in certain definition.
   p.stdin.write((str(b % len(tbl)) + "\n").encode("utf-8"))
   p.stdin.flush()
-  w  = p.stdout.readline().decode("utf-8").split(",")
-  fl = False
-  if(bb == w[1]):
-    s += 1
-    fl = True
-  bb = w[1]
-  f  = tbl[int(float(w[0])) % len(tbl)]
-  if(not fl and (t - s) % int(sys.argv[1]) == 0):
+  w = - float(p.stdout.readline().decode("utf-8").split(",")[0])
+  if(w != 0.):
+    flg = True
+  if(flg):
+    f = tbl[int(w) % len(tbl)]
     track.append(Message('note_on',  note=f, velocity=127, time=0))
     track.append(Message('note_off', note=f, time=120))
-  print(f)
-  t += 1
+    print(w)
+    print(f)
 mid.save('rand_correct.mid')
 
