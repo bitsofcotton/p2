d4448364e6365aebc92248aebfaa1d8577258636
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2020-11-23 17:13:59 +0900

    fix s reuse.

--- p2.py
+++ p2.py
@@ -11,24 +11,24 @@ def getrand():
 p = []
 r = []
 d = []
-for s in range(0, nproc):
+for s in range(0, nproc + 1):
   p.append(subprocess.Popen(pp, stdin=subprocess.PIPE, stdout=subprocess.PIPE))
   r.append(0)
   d.append(0)
 
 bd = 0
 bb = 0
-s  = 0
+ss = 0
 for line in sys.stdin:
   dd = float(line)
   b  = 0
-  for s in range(0, len(p)):
+  for s in range(0, nproc):
     d[s] += (dd - bd) * r[s]
     p[s].stdin.write(str(d[s]) + "\n")
     r[s]  = getrand()
     b    += float(p[s].stdout.readline()) * r[s]
-  s += (dd - bd) * bb
+  ss += (dd - bd) * bb
   bd = dd
   bb = b
-  print s
+  print ss
 
