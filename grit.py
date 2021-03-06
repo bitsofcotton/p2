bcc3377e2b24296ccf3d99a90971febb7885ae62
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-03-07 01:34:51 +0900

    subtract invariant, if we're lucky its continuous.

--- grit.py
+++ grit.py
@@ -2,52 +2,26 @@ import sys
 import subprocess
 import numpy
 
-p = [subprocess.Popen(['./p0', '-30', '1'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)]
-#p  = [subprocess.Popen(['./p1', '0', '-6', '-2', '-3', '500', '1000'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)]
-tp = [[0, 0]]
-tm = [[0, 0]]
-s  = [0]
-M  = [0]
-s0 = s1 = bd = 0
+p = []
+p.append(subprocess.Popen(['./p1', '0', '-12', '-4', '500', '10000'], stdin = subprocess.PIPE, stdout = subprocess.PIPE))
 for t in range(0, int(sys.argv[1])):
-  p.append(subprocess.Popen(['./p0', '-30', '1'], stdin = subprocess.PIPE, stdout = subprocess.PIPE))
-  #p.append(subprocess.Popen(['./p1', '0', '-6', '-2', '-3', '1', '1000'], stdin = subprocess.PIPE, stdout = subprocess.PIPE))
-  tp.append([0, 0])
-  tm.append([0, 0])
-  s.append(0)
-  M.append(0)
+  p.append(subprocess.Popen(['./p1', '0', '-12', '-4', '1', '10000'], stdin = subprocess.PIPE, stdout = subprocess.PIPE))
+p.append(subprocess.Popen(['./p0', '30', '1'], stdin = subprocess.PIPE, stdout = subprocess.PIPE))
+M = s = bd = 0
 for line in sys.stdin:
   try:
-    d   = float(line.split(",")[0])
-    s1 += (d - bd) * M[- 1] * (tp[- 1][0] - tp[- 1][1] - tm[- 1][0] + tm[- 1][1])
-    for u in range(0, len(p)):
-      if(u == 0):
-        p[u].stdin.write(str(d) + "\n")
-      else:
-        p[u].stdin.write(str(s[u - 1]) + "\n")
-      p[u].stdin.flush()
-      if(u == len(p) - 1):
-        s0 += (d - bd) * M[u] * (tp[u][0] - tp[u][1] - tm[u][0] + tm[u][1])
-      s[u] += (d - bd) * M[u] * (tp[u][0] - tp[u][1] - tm[u][0] + tm[u][1])
-      line = p[u].stdout.readline().split(",")
-      tp[u][0] = float(line[4])
-      tm[u][0] = float(line[5])
-      if(tp[u][0] == 0): tp[u][1] = 0
-      if(tm[u][0] == 0): tm[u][1] = 0
-      if(u == 0):
-        M[u] = float(line[0])
-      else:
-        M[u] = M[u - 1] * float(line[0]) * (tp[u - 1][0] - tp[u - 1][1] - tm[u - 1][0] + tm[u - 1][1])
-      M[u] *= 1e12
-    if(0 < s0):
-      s0 = 0
-      for u in range(0, len(tp)):
-        tp[u][1] = tp[u][0]
-        tm[u][1] = tm[u][0]
+    d  = float(line.split(",")[0])
+    s += (d - bd) * M
+    M = 1.
+    for pp in p:
+      pp.stdin.write(str(d) + "\n")
+      pp.stdin.flush()
+      l  = pp.stdout.readline().split(",")
+      M *= float(l[0])
+      d  = float(l[3])
     bd = d
   except:
     continue
-  print s1, ",", M[- 1] * (tp[- 1][0] - tp[- 1][1] - tm[- 1][0] + tm[- 1][1])
-  #print numpy.log(abs(s1))
+  print d, ",", M
   sys.stdout.flush()
 
