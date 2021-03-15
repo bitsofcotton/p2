7f8966966e99dbcba62f6a13040f3de0ca73acdc
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-03-15 17:15:25 +0900

    perverse fix.

--- perverse.py
+++ perverse.py
@@ -2,31 +2,23 @@ import sys
 import subprocess
 import random
 
-p = subprocess.Popen(['./catgp', '8', '.75', '20', '40', '60', '80', '100', '120', '140', '160', '180', '200'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-q = subprocess.Popen(['./p01'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-s = bd = M = MM = 0.
+p = subprocess.Popen(['./p01'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+s = bd = M = 0.
 for line in sys.stdin:
-  try:
+  #try:
     d  = float(line[:- 1].split(",")[0])
     p.stdin.write(str(d) + "\n")
-    p.stdin.flush()
-    q.stdin.write(p.stdout.readline().split(",")[5] + "\n")
-    q.stdin.flush()
-    bM  = M
-    bMM = MM
-    qq  = q.stdout.readline().split(",")
-    M   = float(qq[6])
-    MM  = float(qq[5])
+    bM = M
+    r  = p.stdout.readline().split(",")
+    M  = float(r[6])
     if(bd != 0 and bM != 0):
-      if(bM < M and bMM < MM):
+      if(bM < M):
         s -= (d - bd)
-      elif(M < bM and MM < bMM):
+      elif(M < bM):
         s += (d - bd)
-      else:
-        s += (d - bd) * random.uniform(- 1, 1)
     print s
     bd  = d
     sys.stdout.flush()
-  except:
-    continue
+  #except:
+  #  continue
 
