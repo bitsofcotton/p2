7b8a108a220374c7b24ca31dfdf23c80a994ca86
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-04-24 16:57:23 +0900

    catgp uses delta to compat with catg.hh change

--- catgp.cc
+++ catgp.cc
@@ -16,17 +16,19 @@ typedef myfloat num_t;
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   int   stat(200);
-  num_t inten(- num_t(1) / num_t(2));
+  num_t inten0(- num_t(1) / sqrt(num_t(2)));
+  num_t inten1(- num_t(1) / sqrt(num_t(2)));
   if(argc < 2)
-    std::cerr << "catgp <condition>? <intensity>?" << std::endl;
+    std::cerr << "catgp <condition>? <intensity0>? <intensity1>?" << std::endl;
   else {
-    if(1 < argc) stat  = std::atoi(argv[1]);
+    if(1 < argc) stat   = std::atoi(argv[1]);
     // XXX: atof.
-    if(2 < argc) inten = std::atof(argv[2]);
+    if(2 < argc) inten0 = std::atof(argv[2]);
+    if(3 < argc) inten1 = std::atof(argv[3]);
   }
-  std::cerr << "continue with catgp " << stat << " " << inten << std::endl;
-  P012L<num_t, false> p(2, stat, inten);
-  auto  q(p);
+  std::cerr << "continue with catgp " << stat << " " << inten0 << " " << inten1 << std::endl;
+  P012L<num_t, false> p(2, stat, inten0);
+  P012L<num_t, false> q(2, stat, inten1);
   std::string s;
   num_t d(0);
   auto  s0(d);
@@ -47,9 +49,7 @@ int main(int argc, const char* argv[]) {
           s0 += (bdelta = (d - bd) - M);
         }
       }
-      M0 = p.next(d);
-      if(s2 != num_t(0))
-        M = M0 + q.next(s2);
+      M = (M0 = p.next(d - bd)) + q.next(s2);
       if(! isfinite(M) || isnan(M)) M = num_t(0);
     }
     std::cout << M + bdelta << ", " << s0 << ", " << s1 << std::endl << std::flush;
