c4c449dd9067185cc0e701024573712dd9438931
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-04-25 01:43:24 +0900

    fix catgp.

--- catgp.cc
+++ catgp.cc
@@ -16,14 +16,12 @@ typedef myfloat num_t;
 
 int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
-  int pcount(520);
+  int stat(520);
   if(argc < 2)
-    std::cerr << "catgp <pred_count>?" << std::endl;
-  else if(1 < argc) pcount = std::atoi(argv[1]);
-  std::cerr << "continue with catgp " << pcount << std::endl;
-  std::vector<P012L<num_t, false> > p;
-  for(int i = 8; i < pcount; i *= 2)
-    p.emplace_back(P012L<num_t, false>(2, i));
+    std::cerr << "catgp <condition>?" << std::endl;
+  else if(1 < argc) stat = std::atoi(argv[1]);
+  std::cerr << "continue with catgp " << stat << std::endl;
+  P012L<num_t, false> p(2, stat);
   auto  q(p);
   std::string s;
   num_t d(0);
@@ -32,6 +30,7 @@ int main(int argc, const char* argv[]) {
   auto  s2(d);
   auto  M0(d);
   auto  M(d);
+  auto  bdelta(d);
   while(std::getline(std::cin, s, '\n')) {
     const auto bd(d);
     std::stringstream ins(s);
@@ -40,22 +39,14 @@ int main(int argc, const char* argv[]) {
       if(bd != num_t(0) && M0 != num_t(0)) {
         s2 = (d - bd) - M0;
         if(M != num_t(0)) {
-          s0 += (d - bd) - M;
-          s1 += (d - bd) * M;
+          s1 += (d - bd) * (M + bdelta);
+          s0 += (bdelta = (d - bd) - M);
         }
       }
-      M0 = num_t(0);
-      for(int i = 0; i < p.size(); i ++)
-        M0 += p[i].next(d - bd);
-      M0 /= num_t(p.size());
-      M  = num_t(0);
-      for(int i = 0; i < q.size(); i ++)
-        M  += q[i].next(s2);
-      M /= num_t(p.size());
-      M += M0;
+      M = (M0 = p.next(d - bd)) + q.next(s2);
       if(! isfinite(M) || isnan(M)) M = num_t(0);
     }
-    std::cout << M << ", " << s0 << ", " << s1 << ", " << M0 << std::endl << std::flush;
+    std::cout << M + bdelta << ", " << s0 << ", " << s1 << std::endl << std::flush;
   }
   return 0;
 }
