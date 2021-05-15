0ebdbfc6e39c80fda30f6259cc3585559f2306f4
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-05-15 11:26:35 +0900

    compat with latest catg.

--- catgp.cc
+++ catgp.cc
@@ -17,35 +17,35 @@ int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   int stat(256);
   int var(8);
+  int comp(16);
   if(argc < 2)
-    std::cerr << "catgp <condition>? <context>?" << std::endl;
+    std::cerr << "catgp <condition>? <context>? <complement>?" << std::endl;
   else {
     if(1 < argc) stat = std::atoi(argv[1]);
-    if(2 < argc) var  = std::atoi(argv[2]);
+    if(2 < argc) comp = (var = std::atoi(argv[2])) * 2;
+    if(3 < argc) comp = std::atoi(argv[3]);
   }
-  std::cerr << "continue with catgp " << stat << " " << var << std::endl;
-  P012L<num_t, false> p(stat, var);
+  std::cerr << "continue with catgp " << stat << " " << var << " " << comp << std::endl;
+  P012L<num_t, false> p(abs(stat), var, comp);
+  P012L<num_t, true>  q(abs(stat), var, comp);
   std::string s;
   num_t d(0);
   auto  s0(d);
   auto  s1(d);
-  auto  M0(d);
   auto  M(d);
   while(std::getline(std::cin, s, '\n')) {
     const auto bd(d);
-    const auto nM(M + M0);
     std::stringstream ins(s);
     ins >> d;
     if(d != bd) {
       if(bd != num_t(0) && M != num_t(0)) {
-        s0 += (d - bd) - nM;
-        s1 += (d - bd) * nM;
-        M0  = d - bd - M;
+        s0 += (d - bd) - M;
+        s1 += (d - bd) * M;
       }
-      M  = p.next(d) - d;
+      M  = (stat < 0 ? q.next(d) : p.next(d)) - d;
       if(! isfinite(M) || isnan(M)) M = num_t(0);
     }
-    std::cout << nM << ", " << s0 << ", " << s1 << std::endl << std::flush;
+    std::cout << M << ", " << s0 << ", " << s1 << std::endl << std::flush;
   }
   return 0;
 }
---
+++ plog.py
@@ -0,0 +1,23 @@
+import sys
+import io
+import subprocess
+import numpy
+
+p  = subprocess.Popen(sys.argv[1:], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+bd = dd = M = s = 0.
+with io.open(sys.stdin.fileno(), 'rb', closefd=False) as stdin:
+  for line in stdin:
+    try:
+      d = float(line.decode("utf-8").split(",")[0])
+    except:
+      continue
+    s  += (d - bd) * M
+    dd += ((numpy.arctan(d) + 2.) / (numpy.arctan(bd) + 2.)) - 1.
+    p.stdin.write((str(dd) + "\n").encode("utf-8"))
+    p.stdin.flush()
+    cf = p.stdout.readline().decode("utf-8").split(",")
+    M  = float(cf[0])
+    print(str(M) + ", " + str(s) + ", " + str(cf[2][:- 1]))
+    bd = d
+    sys.stdout.flush()
+
