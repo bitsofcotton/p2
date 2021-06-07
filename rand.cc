0ac31b78152a67002963b5da34014ed3d4ccde72
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-06-07 21:49:10 +0900

    rand.cc option.

--- rand.cc
+++ rand.cc
@@ -17,24 +17,29 @@ int main(int argc, char* argv[]) {
   int t(0);
   while(true) {
     double s(0);
-    for(int i = 0; i < std::atoi(argv[3]); i ++) {
-      double r(0);
-      // XXX: this is not better random, we need sometimes re rand()
-      //      better to get if larger than them.
-      for(int j = 0; j < std::atoi(argv[3]); j ++)
-        r += double((int)(mmt(mt, std::atoi(argv[1]), std::atoi(argv[2])) %
-                            0x8000000) - 0x4000000 + double(1) / double(2)) *
-             double((int)(mmt(mt, std::atoi(argv[1]), std::atoi(argv[2])) %
-                            0x8000000) - 0x4000000 + double(1) / double(2));
-      s += r * double((int)(mmt(mt, std::atoi(argv[1]), std::atoi(argv[2])) %
-             0x8000000) - 0x4000000 + double(1) / double(2));
-    }
-    if((t ++) % 3 == 0) {
-      if(0 <= s)
-        std::cout <<   pow(  s, double(1) / double(3)) << std::endl;
-      else
-        std::cout << - pow(- s, double(1) / double(3)) << std::endl;
-    }
+    if(std::atoi(argv[3])) {
+      for(int i = 0; i < std::atoi(argv[3]); i ++) {
+        double r(0);
+        // XXX: this is not better random, we need sometimes re rand()
+        //      better to get if larger than them.
+        for(int j = 0; j < std::atoi(argv[3]); j ++)
+          r += double((int)(mmt(mt, std::atoi(argv[1]), std::atoi(argv[2])) %
+                              0x8000000) - 0x4000000 + double(1) / double(2)) *
+               double((int)(mmt(mt, std::atoi(argv[1]), std::atoi(argv[2])) %
+                              0x8000000) - 0x4000000 + double(1) / double(2));
+        s += r * double((int)(mmt(mt, std::atoi(argv[1]), std::atoi(argv[2])) %
+               0x8000000) - 0x4000000 + double(1) / double(2));
+      }
+      if((t ++) % 3 == 0) {
+        if(0 <= s)
+          std::cout <<   pow(  s, double(1) / double(3)) << std::endl;
+        else
+          std::cout << - pow(- s, double(1) / double(3)) << std::endl;
+      }
+    } else
+      std::cout <<
+        double((int)(mmt(mt, std::atoi(argv[1]), std::atoi(argv[2])) %
+                       0x8000000) - 0x4000000 + double(1) / double(2)) << std::endl;
   }
   return 0;
 }
