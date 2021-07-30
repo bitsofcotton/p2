6ad80c2c2d42654ddce0d05a944c5a2df82b1251
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-07-30 22:13:51 +0900

    fix continuity on input.

---
+++ cont.py
@@ -0,0 +1,28 @@
+import sys
+import io
+
+def ifloat(x):
+  try:
+    return float(x)
+  except:
+    try:
+      b = x.split("*")
+      return float(b[0]) * pow(2., float(b[1][2:]))
+    except:
+      pass
+  return 0.
+
+bd = bbd = 0
+y  = vy = 1.
+for line in io.open(sys.stdin.fileno(), 'rb', closefd = False):
+  d  = ifloat(line.decode("utf-8")[:- 1].split(",")[int(sys.argv[1])])
+  by = y
+  if(d != 0.):
+    bvy = vy
+    vy  = (d - bd + bbd) / d
+    y  *= vy / bvy
+  print(d * y + bd * by)
+  bbd = bd
+  bd  = d
+  sys.stdout.flush()
+
