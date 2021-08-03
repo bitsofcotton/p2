218adcec6b7663b3def4ce3534af88e729325ed8
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-08-03 20:20:23 +0900

    revert to use cdot on output.

--- catgp.cc
+++ catgp.cc
@@ -53,7 +53,7 @@ int main(int argc, const char* argv[]) {
     if(d != bd) {
       if(M != num_t(0)) {
         s0 += (s3 = (d - bd) - (M - bd));
-        s1 += (s2 = (M - bd) / (d - bd));
+        s1 += (s2 = (d - bd) * (M - bd));
       }
       M  = tan(var < 0 ? (stat < 0 ? qw.next(atan(d)) : qp.next(atan(d)))
                        : (stat < 0 ? pw.next(atan(d)) : pp.next(atan(d))) );
