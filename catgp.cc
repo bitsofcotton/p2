0280be0c3d9303445f19a9d5957001f4af0b339e
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-08-18 22:47:24 +0900

    origin fix.

--- catgp.cc
+++ catgp.cc
@@ -49,8 +49,8 @@ int main(int argc, const char* argv[]) {
     ins >> d;
     if(d != num_t(0)) {
       if(M != num_t(0)) {
-        s0 += (s3 = (d - bd) - (M - d));
-        s1 += (s2 = (d - bd) * (M - d));
+        s0 += (s3 = (d - bd) - (M - bd));
+        s1 += (s2 = (d - bd) * (M - bd));
       }
       M  = tan(stat < 0 ? q.next(atan(d)) : p.next(atan(d)));
       if(! isfinite(M) || isnan(M)) M = num_t(0);
