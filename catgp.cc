47758c82de965a743e69d2a8074bc29e04be88f6
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-08-29 20:04:32 +0900

    additional steps.

--- catgp.cc
+++ catgp.cc
@@ -32,28 +32,30 @@ int main(int argc, const char* argv[]) {
   std::cout << std::setprecision(30);
   int  stat(256);
   int  var(2);
-  int  pw(1);
+  int  step(1);
   if(argc < 2)
-    std::cerr << "catgp <condition>? <var>? <pow>?" << std::endl;
+    std::cerr << "catgp <condition>? <var>? <step>?" << std::endl;
   else {
     if(1 < argc) stat = std::atoi(argv[1]);
     if(2 < argc) var  = std::atoi(argv[2]);
-    if(3 < argc) pw   = std::atoi(argv[3]);
+    if(3 < argc) step = std::atoi(argv[3]);
   }
-  std::cerr << "continue with catgp " << stat << " " << var << " " << pw << std::endl;
-  P012L<num_t, linearFeeder<num_t, sumFeeder<num_t> > > p(abs(stat), var);
-  P012L<num_t, arctanFeeder<num_t, sumFeeder<num_t> > > q(abs(stat), var);
+  std::cerr << "continue with catgp " << stat << " " << var << " " << step << std::endl;
+  P012L<num_t, linearFeeder<num_t, idFeeder<num_t> > > p(abs(stat), var, step);
+  P012L<num_t, arctanFeeder<num_t, idFeeder<num_t> > > q(abs(stat), var, step);
   std::string s;
   num_t d(0);
-  auto  M(d);
   auto  D(d);
+  std::vector<num_t> M;
+  M.resize(step, num_t(0));
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
-    D = d * M;
-    M = stat < 0 ? q.next(d) : p.next(d);
-    M = pw == 0 ? sgn<num_t>(M) : sgn<num_t>(M) * pow(abs(M), pw < 0 ? num_t(1) / num_t(abs(pw)) : num_t(pw));
-    std::cout << D << ", " << M << ", " << d << std::endl << std::flush;
+    D  = d * M[0];
+    for(int i = 1; i < M.size(); i ++)
+      M[i - 1] = M[i];
+    M[M.size() - 1] = stat < 0 ? q.next(d) : p.next(d);
+    std::cout << D << ", " << M[M.size() - 1] << ", " << d << std::endl << std::flush;
   }
   return 0;
 }
