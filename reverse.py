aac063daa9cd1ceb4871399b751df96bc9c546e7
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-09-11 23:52:25 +0900

    Delete reverse.py

--- reverse.py
+++
@@ -1,9 +0,0 @@
-import sys
-import io
-
-arr = []
-for line in io.open(sys.stdin.fileno(), 'rb', closefd = False):
-  arr.append(line[:- 1].decode("utf-8"))
-for t in range(0, len(arr)):
-  print(arr[- t - 1])
-
