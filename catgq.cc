2643132e9e1d20aa6f5cf18c665707098568d074
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-09-14 23:00:25 +0900

    rewrite p0123
    my key input has exchanged order in the case hard context switch.

--- catgq.cc
+++ catgq.cc
@@ -25,29 +25,30 @@ public:
   inline P0123() { ; }
   inline P0123(const int& recur) { 
     sh.resize(abs(recur), T(0));
-    Mh.resize(abs(recur), T(0));
-    mh.resize(abs(recur), T(0));
+    Mh.resize(sh.size(), T(0));
+    mh.resize(sh.size(), T(0));
     if((this->recur = recur) < 0) {
-      p1 = P012L<T, arctanFeeder<T, idFeeder<T> > >(625 * 2, 5);
-      q1 = P0<T, arctanFeeder<T, sumFeeder<T, idFeeder<T> > > >(25);
+      p1.resize(sh.size(), P012L<T, arctanFeeder<T, idFeeder<T> > >(pow(6, 4), 5));
+      q1.resize(sh.size(), P0<T, arctanFeeder<T, idFeeder<T> > >(25));
     } else {
-      p0 = P012L<T, linearFeeder<T, idFeeder<T> > >(625 * 2, 5);
-      q0 = P0<T, linearFeeder<T, sumFeeder<T, idFeeder<T> > > >(25);
+      p0.resize(sh.size(), P012L<T, linearFeeder<T, idFeeder<T> > >(pow(6, 4), 5));
+      q0.resize(sh.size(), P0<T, linearFeeder<T, idFeeder<T> > >(25));
     }
+    bM = S = T(t ^= t);
   }
   inline ~P0123() { ; }
   inline T next(const T& in, const T& d) {
     for(int i = 1; i < sh.size(); i ++) sh[i - 1] = move(sh[i]);
-    sh[sh.size() - 1] = in;
-    const auto feed(d * bM < T(0) ? - sqrt(abs(d * bM)) : sqrt(abs(d * bM)));
-    const auto resq(recur < 0 ? q1.next(feed) : q0.next(feed));
     for(int i = 1; i < Mh.size(); i ++) Mh[i - 1] = move(Mh[i]);
-    Mh[Mh.size() - 1] = (recur < 0 ? p1.next(in - sh[0]) : p0.next(in - sh[0])) + in;
     for(int i = 1; i < mh.size(); i ++) mh[i - 1] = move(mh[i]);
+    sh[sh.size() - 1] = in;
+    Mh[Mh.size() - 1] = (recur < 0 ? p1[t % p1.size()].next(in - sh[0]) : p0[t % p0.size()].next(in - sh[0])) + in;
     mh[mh.size() - 1] = T(0);
     for(int i = 0; i < Mh.size(); i ++)
       mh[mh.size() - 1] += Mh[i];
     mh[mh.size() - 1] /= T(Mh.size());
+    S += d * bM < T(0) ? - sqrt(abs(d * bM)) : sqrt(abs(d * bM));
+    const auto resq(recur < 0 ? q1[t % q1.size()].next(S) : q0[t % q0.size()].next(S));
     bM = T(0);
     T denom(0);
     for(int i = 0; i < mh.size(); i ++) {
@@ -55,17 +56,20 @@ public:
       denom += T(1) / T(mh.size() - i);
     }
     bM /= denom;
+    t ++;
     return bM < T(0) ? - resq : resq;
   }
   vector<T> sh;
   vector<T> Mh;
   vector<T> mh;
   T   bM;
+  T   S;
   int recur;
-  P012L<T, linearFeeder<T, idFeeder<T> > > p0;
-  P012L<T, arctanFeeder<T, idFeeder<T> > > p1;
-  P0<T, linearFeeder<T, sumFeeder<T, idFeeder<T> > > > q0;
-  P0<T, arctanFeeder<T, sumFeeder<T, idFeeder<T> > > > q1;
+  int t;
+  vector<P012L<T, linearFeeder<T, idFeeder<T> > > > p0;
+  vector<P012L<T, arctanFeeder<T, idFeeder<T> > > > p1;
+  vector<P0<T, linearFeeder<T, idFeeder<T> > > > q0;
+  vector<P0<T, arctanFeeder<T, idFeeder<T> > > > q1;
 };
 
 /*
@@ -90,56 +94,26 @@ int main(int argc, const char* argv[]) {
   // c1 = (percent - .5) / (1.5 - sqrt(2))
   //    ~ sqrt(2n + 1) / sqrt(pi)
   // <=> n == (((percent - .5) / (1.5 - sqrt(2)))^2 * pi - 1) / 2
-  const int  n((pow((num_t(abs(percent)) / num_t(100) - num_t(1) / num_t(2)) / (num_t(3) / num_t(2) - sqrt(num_t(2))), num_t(2)) * atan(num_t(1)) * num_t(4) - num_t(1)) / num_t(2));
-  const auto nn((n >> 1) + (n & 1));
-  std::cerr << "continue with catgq " << percent << "%% uses: " << n << std::endl;
+  const auto n(max(int((pow((num_t(abs(percent)) / num_t(100) - num_t(1) / num_t(2)) / (num_t(3) / num_t(2) - sqrt(num_t(2))), num_t(2)) * atan(num_t(1)) * num_t(4) - num_t(1)) / num_t(2)), int(1)));
+  std::cerr << "continue with catgq " << percent << "\% uses: " << n << std::endl;
   std::vector<P0123<num_t> > p;
-  if(0 < percent) {
-    p.reserve(nn << 1);
-    for(int i = 1; i <= nn; i ++) {
-      p.emplace_back(P0123<num_t>(  i + 1));
-      p.emplace_back(P0123<num_t>(- i - 1));
-    }
-  } else {
-    p.emplace_back(P0123<num_t>(  percent));
-    p.emplace_back(P0123<num_t>(- percent));
-  }
-  std::vector<P0<num_t, linearFeeder<num_t, idFeeder<num_t> > > > q0;
-  std::vector<P0<num_t, arctanFeeder<num_t, idFeeder<num_t> > > > q1;
-  q0.resize(percent < 0 ? abs(percent) : nn + 1, P0<num_t, linearFeeder<num_t, idFeeder<num_t> > >(25));
-  q1.resize(percent < 0 ? abs(percent) : nn + 1, P0<num_t, arctanFeeder<num_t, idFeeder<num_t> > >(25));
+  p.reserve(n);
+  for(int i = 2; i < n + 2; i ++)
+    p.emplace_back(P0123<num_t>(percent < 0 ? - i :  i));
   std::string s;
   num_t d(0);
   auto  D(d);
-  auto  D0(d);
   auto  S(d);
-  auto  S0(d);
   auto  SS(d);
   auto  M(d);
-  auto  M0(d);
-  std::vector<num_t> MM;
-  MM.resize(q0.size(), d);
-  const auto twoPi(atan(num_t(1)) * num_t(8));
-  int   ii(0);
   while(std::getline(std::cin, s, '\n')) {
     std::stringstream ins(s);
     ins >> d;
-    D0 = d * M;
-    D  = d * M * M0;
-    M  = num_t(0);
-    S += (d = atan(d));
-    for(int i = 0; i < p.size(); i ++)
+    D  = d * M;
+    M  = p[0].next(S += d, d);
+    for(int i = 1; i < p.size(); i ++)
       M += p[i].next(S, d);
-    M /= num_t(p.size());
-    if(twoPi < abs(M)) M = num_t(0);
-    for(int i = 1; i < MM.size(); i ++) MM[i - 1] = std::move(MM[i]);
-    ii  = (ii + 1) % q0.size();
-    S0 += D0;
-    MM[MM.size() - 1] = (q0[ii].next(S0) + q1[ii].next(S0)) / num_t(2);
-    M0 = num_t(0);
-    for(int i = 0; i < MM.size(); i ++)
-      M0 += MM[i];
-    std::cout << D << ", " << M * M0 << ", " << d << ", " << S << ", " << (SS += D) << std::endl << std::flush;
+    std::cout << D << ", " << (M /= num_t(p.size())) << ", " << d << ", " << S << ", " << (SS += D) << std::endl << std::flush;
   }
   return 0;
 }
