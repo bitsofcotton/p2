486cf8b06ef40efb81e29320eefb453b6a806d61
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-11-24 21:44:57 +0900

    speed hack.

--- pred.py
+++ pred.py
@@ -2,54 +2,61 @@ import sys
 import subprocess
 from PIL import Image
 import numpy as np
-img = []
+dimg = []
+w = h = 0
 for line in sys.stdin:
   try:
-    img.append(Image.open(line[:- 1]))
+    img = Image.open(line[:- 1])
+    if(w != 0 and h != 0 and img.size != (w, h)):
+      print("NG", img.size[0], w, img.size[1], h)
+    w = img.size[0]
+    h = img.size[1]
+    if(w == 0 or h == 0): continue
+    dimg.append([])
+    for x in range(0, w):
+      dimg[- 1].append([])
+      for y in range(0, h):
+        pp = img.getpixel((x, y))
+        if(not isinstance(pp, tuple) or len(pp) <= 1):
+          pp = (pp, pp, pp)
+        elif(len(pp) < 3):
+          pp = (pp[0], pp[1 % len(pp)], pp[2 % len(pp)])
+        dimg[- 1][- 1].append(pp)
   except:
     pass
 pixels = []
-out = Image.new("RGB", (img[0].size[0], img[0].size[1]), (256, 256, 256))
 m = M = 0.
 def ps(x, p):
-  if(x < 0): return - pow(- x, p)
-  if(0 < x): return   pow(  x, p)
-  return 0.
-for x in range(0, img[0].size[0]):
+  if(not np.isfinite(x)): return 0.
+  elif(x < 0): return - pow(- x, p)
+  return pow(x, p)
+p0 = subprocess.Popen(sys.argv[1:], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+p1 = subprocess.Popen(sys.argv[1:], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+p2 = subprocess.Popen(sys.argv[1:], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+for x in range(0, w):
   pixels.append([])
-  for y in range(0, img[0].size[1]):
-    d = [0., 0., 0.]
-    p0 = subprocess.Popen(sys.argv[1:], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    p1 = subprocess.Popen(sys.argv[1:], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    p2 = subprocess.Popen(sys.argv[1:], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    t = 0
-    for idx in range(0, len(img)):
-      pp = img[idx].getpixel((x, y))
-      if(not isinstance(pp, tuple)):
-        pp = [pp, pp, pp]
-      p0.stdin.write((str(pp[0]) + "\n").encode("utf-8"))
-      p1.stdin.write((str(pp[1]) + "\n").encode("utf-8"))
-      p2.stdin.write((str(pp[2]) + "\n").encode("utf-8"))
+  for y in range(0, h):
+    d = (0., 0., 0.)
+    for idx in range(0, len(dimg)):
+      p0.stdin.write((str(dimg[idx][x][y][0]) + "\n").encode("utf-8"))
+      p1.stdin.write((str(dimg[idx][x][y][1]) + "\n").encode("utf-8"))
+      p2.stdin.write((str(dimg[idx][x][y][2]) + "\n").encode("utf-8"))
       p0.stdin.flush()
       p1.stdin.flush()
       p2.stdin.flush()
-      t += 1
-      if((t - 2) % 3 == 0):
+      if((idx - 2) % 3 == 0):
         pp0 = p0.stdout.readline().decode("utf-8").split(",")
         pp1 = p1.stdout.readline().decode("utf-8").split(",")
         pp2 = p2.stdout.readline().decode("utf-8").split(",")
-        if(float(pp0[4]) != 0 and float(pp1[4]) != 0 and float(pp2[4]) != 0):
-          d = [ps(float(pp0[1]) * float(pp0[3]), .5) / float(pp0[4]) / 2. + pp[0],
-               ps(float(pp1[1]) * float(pp1[3]), .5) / float(pp1[4]) / 2. + pp[1],
-               ps(float(pp2[1]) * float(pp2[3]), .5) / float(pp2[4]) / 2. + pp[2] ]
+        if(4 < len(pp0) and 4 < len(pp1) and 4 < len(pp2) and \
+           float(pp0[4]) != 0 and float(pp1[4]) != 0 and float(pp2[4]) != 0):
+          d = (ps(float(pp0[1]) * float(pp0[3]), .5) / float(pp0[4]) / 2. + dimg[idx][x][y][0],
+               ps(float(pp1[1]) * float(pp1[3]), .5) / float(pp1[4]) / 2. + dimg[idx][x][y][1],
+               ps(float(pp2[1]) * float(pp2[3]), .5) / float(pp2[4]) / 2. + dimg[idx][x][y][2] )
         else:
-          d = [0, 0, 0]
-    p0.stdin.close()
-    p1.stdin.close()
-    p2.stdin.close()
-    p0.kill()
-    p1.kill()
-    p2.kill()
+          d = (0, 0, 0)
+        if(len(dimg) - idx - 1 < 3):
+          break
     print(d)
     if(np.isfinite(d[0])):
       M = max(d[0], M)
@@ -60,9 +67,10 @@ for x in range(0, img[0].size[0]):
     if(np.isfinite(d[2])):
       M = max(d[2], M)
       m = min(d[2], m)
-    pixels[- 1].append(d)
-for x in range(0, len(pixels)):
-  for y in range(0, len(pixels[x])):
+    pixels[- 1].append(list(d))
+out = Image.new("RGB", (w, h), (256, 256, 256))
+for x in range(0, w):
+  for y in range(0, h):
     for idx in range(0, 3):
       pixels[x][y][idx] -= m
       if(M == m or not np.isfinite(pixels[x][y][idx])):
