f2ecf83766b6dd734504d56491668a7c550fb9da
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-02-05 00:48:59 +0900

    revert.

--- d.py
+++ d.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 import io
 import sys
-import random
 import subprocess
 
 def ifloat(x):
@@ -17,8 +16,7 @@ def ifloat(x):
 
 p = subprocess.Popen(sys.argv[2:], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 q = subprocess.Popen(sys.argv[2:], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-M = M0 = t = 0
-b = []
+M = M0 = S = SS = t = 0
 H = []
 h = []
 g = []
@@ -27,20 +25,15 @@ for t in range(0, int(sys.argv[1])):
   h.append(0.)
   g.append(0.)
 for line in io.open(sys.stdin.fileno(), 'r', encoding='utf-8', closefd=False):
-  b.append(float(line.split(",")[0]))
-  if(t % 2 == 0 and int(sys.argv[1]) * 2 < len(b)):
-    b = b[- int(sys.argv[1]) * 2:]
-    S = SS = 0.
-    for u in range(0, len(H)):
-      H[- u - 1] = 0.
-      for v in range(0, 2):
-        g[- u - 1]  = h[- u - 1]
-        H[- u - 1] += SS
-        h[- u - 1]  = SS * 2.
-        S  -= b[- u * 2 - v - 1]
-        SS -= S
-    D = M * (H[- 1] - h[- 1])
-    q.stdin.write((str(M0 * (H[- 1] - h[- 1])) + "\n").encode("utf-8"))
+  S  += float(line.split(",")[0])
+  SS += S
+  g[- 1]  = h[- 1]
+  H[- 1] += SS
+  h[- 1]  = SS * 2.
+  if(t % 2 == 0):
+    # XXX: -S++ - S-
+    D = M * (H[- 1] - h[- 1] + g[- 2] - H[- 2])
+    q.stdin.write((str(M0 * (H[- 1] - h[- 1] + g[- 2] - H[- 2])) + "\n").encode("utf-8"))
     q.stdin.flush()
     for u in range(1, len(H)):
       p.stdin.write((str(H[u] - h[u] + g[- 1] - H[- 1]) + "\n").encode("utf-8"))
@@ -49,6 +42,12 @@ for line in io.open(sys.stdin.fileno(), 'r', encoding='utf-8', closefd=False):
     M0 = M
     M *= ifloat(q.stdout.readline().decode("utf-8").split(",")[1])
     print(D, ", ", M)
+    H = H[1:]
+    h = h[1:]
+    g = g[1:]
+    H.append(0)
+    h.append(0)
+    g.append(0)
   sys.stdout.flush()
   t += 1
 
