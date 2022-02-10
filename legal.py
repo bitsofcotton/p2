7cd3a2819b1d8e8986a4f7fdb980c182f59e9cb7
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-02-10 21:17:48 +0900

    fix legal.py logic.

--- legal.py
+++ legal.py
@@ -19,17 +19,18 @@ p = []
 for u in range(0, int(len(sys.argv) - 3)):
   p.append(subprocess.Popen([sys.argv[2], sys.argv[3 + u]], stdin = subprocess.PIPE, stdout = subprocess.PIPE))
 for line in sys.stdin:
-  d = ifloat(line.split(",")[0])
-  D = d
+  D = ifloat(line.split(",")[0])
+  bM = M
   M = 1.
-  for u in range(0, len(p)):
-    p[u].stdin.write((str(int(D)) + "\n").encode("utf-8"))
-    p[u].stdin.flush()
-    buf  = p[u].stdout.readline().decode("utf-8").split(",")
-    D    = ifloat(buf[0])
-    M   *= ifloat(buf[1])
+  for pp in p:
+    pp.stdin.write((str(int(D)) + "\n").encode("utf-8"))
+    pp.stdin.flush()
+    buf = pp.stdout.readline().decode("utf-8").split(",")
+    D   = ifloat(buf[0])
+    M  *= ifloat(buf[1])
   h.append(abs(M))
   h = h[- int(sys.argv[1]):]
   if(sorted(h)[int(len(h) / 2)] * 2. < abs(M)): M = 0.
+  if(bM == 0.): D = 0.
   print(D, ",", M)
 
