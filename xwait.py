3fad5815b1d1995433cf1f9aa32cd452af3df1ce
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-05-21 00:35:03 +0900

    fix, ok.

--- xwait.py
+++ xwait.py
@@ -7,13 +7,13 @@ import subprocess
 p  = subprocess.Popen(sys.argv[2:], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
 h  = [0]
 g  = [0]
-tt = M = S = 0
+tt = M = S = d = 0
 bd = 1
 for line in io.open(sys.stdin.fileno(), encoding = "utf-8", closefd = False):
-  d  = float(line.split(",")[0])
-  D  = abs(M * h[- 1]) * d * pow(- 1., tt)
-  S += D
+  d0 = float(line.split(",")[0])
+  d += d0
   if(d * bd < 0.):
+    D  = abs(M * h[- 1]) * d * pow(- 1., tt)
     p.stdin.write((str(d) + "\n").encode("utf-8"))
     p.stdin.flush()
     h.append(float(p.stdout.readline().decode("utf-8").split(",")[1]))
@@ -31,7 +31,11 @@ for line in io.open(sys.stdin.fileno(), encoding = "utf-8", closefd = False):
     g  = g[- int(sys.argv[1]):]
     tt += 1
   elif(abs(M * h[- 1]) / 2. < abs(d) and d * pow(- 1., tt) < 0):
+    D  = abs(M * h[- 1]) * d * pow(- 1., tt)
     M  = 0
+  else:
+    D  = 0
+  S += D
   print(D, ",", abs(M * h[- 1]) * pow(- 1., tt), ",", S)
   sys.stdout.flush()
 
