9e287b217f7bd82f95335b8acb8bfb52bd2a518e
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-06-01 19:03:41 +0900

    the integrity doesn't supported even one binaried.
    rand output to file then predict from file works better.
    if the pipe/redirect is cracked, even in one binaried package, we cannot avoid jammers to rewrite output (sign).

--- p2100.cc
+++ p2100.cc
@@ -36,11 +36,9 @@ int main(int argc, const char* argv[]) {
 #endif
 */
   std::cout << std::setprecision(30);
-  int status(20);
-  int mode(- 1);
-  if(argc < 2) std::cerr << argv[0] << " <status>? <mode>? : continue with ";
+  int status(60);
+  if(argc < 2) std::cerr << argv[0] << " <status>? : continue with ";
   if(1 < argc) status = std::atoi(argv[1]);
-  if(2 < argc) mode = std::atoi(argv[2]);
   std::cerr << argv[0] << " " << status << std::endl;
   assert(status);
   const auto var0(max(int(1), int(exp(sqrt(log(num_t(abs(status) * 2)))))));
@@ -50,10 +48,6 @@ int main(int argc, const char* argv[]) {
   shrinkMatrix<num_t, plin1_t> q(plin1_t(abs(status), var1, var1), var1);
   P0maxRank<num_t> r(abs(status) * 2, var0);
   auto  s(r);
-  std::random_device rd;
-  std::mt19937_64 mt(rd());
-  std::shuffle_order_engine<std::linear_congruential_engine<unsigned int, 16807, 0, 2147483647>, 8> kb(rd());
-  std::ranlux48 rl48(rd());
   std::string str;
   num_t zero(int(0));
   auto  d(zero);
@@ -66,30 +60,9 @@ int main(int argc, const char* argv[]) {
   auto  MMr(d);
   auto  MMs(d);
   auto  S(d);
-  while(1) {
-    if(mode < 0) {
-      if(! std::getline(std::cin, str, '\n')) break;
-      std::stringstream ins(str);
-      ins >> d;
-    } else {
-      for(int m = 0; m <= mode / 0x04; m ++) switch(mode & 0x03) {
-      case 0:
-        d = num_t(arc4random() & 0x7fffff) / (num_t(int(0x7fffff)) / num_t(int(2))) - num_t(int(1));
-        break;
-      case 1:
-        d = num_t(int(mt()) & 0x7fffff) / (num_t(int(0x7fffff)) / num_t(int(2))) - num_t(int(1));
-        break;
-      case 2:
-        d = num_t(int(kb()) & 0x7fffff) / (num_t(int(0x7fffff)) / num_t(int(2))) - num_t(int(1));
-        break;
-      case 3:
-        d = num_t(int(rl48()) & 0x7fffff) / (num_t(int(0x7fffff)) / num_t(int(2))) - num_t(int(1));
-        break;
-      default:
-        assert(0 && "Should not be reached.");
-      }
-      mode &= 0x03;
-    }
+  while(std::getline(std::cin, str, '\n')) {
+    std::stringstream ins(str);
+    ins >> d;
     const auto Dp(d * Mp);
     const auto Dq(d * Mp * Mq);
     const auto Dr(d * Mp * Mq * Mr);
@@ -100,11 +73,15 @@ int main(int argc, const char* argv[]) {
     MMs = max(MMs, abs(Dr) * num_t(2 * var0));
     if(status < 0) { Mp = Mq = num_t(int(1)); }
     else {
-      if(abs(Mp = max(- MMp, min(MMp, p.next(d ) )) ) == MMp) Mp = zero;
-      if(abs(Mq = max(- MMq, min(MMq, q.next(Dp) )) ) == MMq) Mq = zero;
+      if(abs(Mp = max(- MMp, min(MMp, p.next(d ) )) ) == MMp)
+        Mp = zero;
+      if(Mp == zero || abs(Mq = max(- MMq, min(MMq, q.next(Dp) )) ) == MMq)
+        Mq = zero;
     }
-    if(abs(Mr = max(- MMr, min(MMr, r.next(Dq)[0] )) ) == MMr) Mr = zero;
-    if(abs(Ms = max(- MMs, min(MMs, s.next(Dr)[0] )) ) == MMs) Ms = zero;
+    if(Mq == zero || abs(Mr = max(- MMr, min(MMr, r.next(Dq)[0] )) ) == MMr)
+      Mr = zero;
+    if(Mr == zero || abs(Ms = max(- MMs, min(MMs, s.next(Dr)[0] )) ) == MMs)
+      Ms = zero;
     std::cout << D << ", " << (Mp * Mq * Mr * Ms) << ", " << (S += D) << std::endl << std::flush;
   }
   return 0;
