159af4cbb0f7e93afc2f4767ca3dd963fd9b6957
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-08-02 17:33:23 +0900

    revert, we still need +infty, 0, 1 state pred.
    from numerical tests.

--- p.cc
+++ p.cc
@@ -24,59 +24,63 @@ public:
     var0 = max(T(int(1)), T(int(exp(sqrt(log(T(status)))))) );
     const auto var1(max(T(int(2)), pow(T(status), T(int(1)) / T(int(3)))));
     const auto var2(max(T(int(2)), pow(T(status), T(int(1)) / T(int(4)))));
-    M.resize(5, Mx0 = MM = T(int(0)));;
+    M.resize(8, Mx0 = MM = T(int(0)));;
     Mx.resize(M.size(), MM);
     p0 = P0maxRank<T>(status);
     p1 = shrinkMatrix<T, P1I<T, idFeeder<T> > >(P1I<T, idFeeder<T> >(status, var1, var1), var1);
     p2 = shrinkMatrix<T, P012L<T, idFeeder<T> > >(P012L<T, idFeeder<T> >(status, var2, var2), var2);
     q  = idFeeder<T>(status);
+    q0 = SimpleVector<T>(status + 1).O();
+    f  = idFeeder<T>(status);
   }
   inline ~P() { ; }
   inline const T& next(T d) {
     Mx0   = max(Mx0, abs(d));
     if(Mx0 == T(int(0)) || (d /= Mx0) == T(int(0)) || ! isfinite(d))
       return MM;
-    M[0]  = max(- T(int(1)), min(T(int(1)), p0.next(d) / var0));
-    M[1]  = max(- T(int(1)), min(T(int(1)), p1.next(d)));
-    M[2]  = max(- T(int(1)), min(T(int(1)), p2.next(d)));
+    Mx[0] = max(Mx[0], abs(d));
+    M[0]  =  - d / Mx[0];
+    M[1]  = max(- Mx[0], min(Mx[0], p1.next(d)));
+    M[1] /= (Mx[1] = max(Mx[1], abs(M[1])));
+    M[2]  = max(- Mx[0], min(Mx[0], p2.next(d)));
+    M[2] /= (Mx[2] = max(Mx[2], abs(M[2])));
+    M[3]  = p0.next(d) / var0;
+    M[3] /= (Mx[3] = max(Mx[3], abs(M[3])));
     {
-      const auto& qq0(q.next(d));
-      Mx[4] = max(abs(d), Mx[4]);
-      if(q.full) {
-        const int qs(sqrt(T(int(qq0.size()))));
-        SimpleVector<T> q0(qs + 1);
-        q0.O();
-        for(int i = 0; i < qq0.size() - qs - 1; i ++) {
-          auto qm0(SimpleVector<T>(qs).O().setVector(0, qq0.subVector(i, qs - 1)));
-          qm0[qm0.size() - 1] = qm0[qm0.size() - 2];
-          auto qm(makeProgramInvariant<T>(qm0));
-          q0 += move(qm.first) *
-            pow(qm.second, ceil(- log(SimpleMatrix<T>().epsilon())));
-        }
-        auto qm0(SimpleVector<T>(qs).O().setVector(0, qq0.subVector(qq0.size() - qs - 1, qs - 1)));
-        qm0[qm0.size() - 1] = qm0[qm0.size() - 2];
-        auto qm(makeProgramInvariant<T>(qm0));
-        M[3] = revertProgramInvariant<T>(make_pair(
-          - (q0.dot(qm.first) - q0[q0.size() - 2] *
-               qm.first[qm.first.size() - 2]) / q0[q0.size() - 2],
-             qm.second)) /
-            pow(qm.second, ceil(- log(SimpleMatrix<T>().epsilon())));
-        M[4] = qq0[0];
-        for(int i = 1; i < qq0.size(); i ++) M[4] += qq0[i];
-        M[4] = - M[4];
+      auto qm(makeProgramInvariant<T>(q.next(d)));
+      q0 += std::move(qm.first) * pow(qm.second, ceil(- log(SimpleMatrix<T>().epsilon())));
+      auto qq(q);
+      auto qqm(makeProgramInvariant<T>(qq.next(d)));
+      M[4] = revertProgramInvariant<T>(make_pair(
+        - (q0.dot(qqm.first) - q0[q0.size() - 2] *
+             qqm.first[qqm.first.size() - 2]) / q0[q0.size() - 2],
+           qqm.second)) /
+          pow(qqm.second, ceil(- log(SimpleMatrix<T>().epsilon())));
+    }
+    M[4] /= (Mx[4] = max(Mx[4], abs(M[4])));
+    M5   -= d;
+    M[5]  = M5 / (Mx[5] = max(Mx[5], abs(M5)));
+    M[6]  = T(int(1)) / T(int(2));
+    {
+      const auto& ff(f.next(d));
+      Mx[7] = max(abs(d), Mx[7]);
+      if(f.full) {
+        M[7] = T(int(0));
+        for(int i = 0; i < ff.size(); i ++) M[7] += ff[i];
+        M[7] = (- M[7]) / (Mx[7] = max(Mx[7], abs(M[7])));
       }
     }
-    MM = T(int(0));
-    for(int i = 0; i < M.size(); i ++) if(isfinite(M[i])) {
-      Mx[i] = max(Mx[i], abs(M[i]));
-      if(Mx[i] != T(int(0))) MM += (M[i] /= Mx[i]);
-    }
-    return MM *= Mx0 / T(int(5));
+    MM    = T(int(0));
+    for(int i = 0; i < M.size(); i ++) if(isfinite(M[i])) MM += M[i];
+    return MM *= Mx0 / T(int(8));
   }
   P0maxRank<T> p0;
   shrinkMatrix<T, P1I<T, idFeeder<T> > > p1;
   shrinkMatrix<T, P012L<T, idFeeder<T> > > p2;
   idFeeder<T> q;
+  SimpleVector<T> q0;
+  idFeeder<T> f;
+  T M5;
   T MM;
   T Mx0;
   T var0;
