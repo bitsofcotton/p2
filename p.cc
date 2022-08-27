6e2b54a19bae277aa7865808edd5ea86723c20b1
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-08-27 21:57:17 +0900

    brush, fast converge, no add .5.

--- p.cc
+++ p.cc
@@ -26,7 +26,7 @@ public:
     var0 = max(T(int(1)), T(int(exp(sqrt(log(T(status)))))) );
     const auto var1(max(T(int(2)), pow(T(status), T(int(1)) / T(int(3)))));
     const auto var2(max(T(int(2)), pow(T(status), T(int(1)) / T(int(4)))));
-    M.resize(8, M5 = Mx0 = MM = T(int(0)));;
+    M.resize(7, M5 = Mx0 = MM = T(int(0)));;
     Mx.resize(M.size(), MM);
     p0 = P0maxRank<T>(status);
     p1 = shrinkMatrix<T, P1I<T, idFeeder<T> > >(P1I<T, idFeeder<T> >(status, var1, var1), var1);
@@ -40,13 +40,12 @@ public:
     Mx0   = max(Mx0, abs(d));
     if(Mx0 == T(int(0)) || (d /= Mx0) == T(int(0)) || ! isfinite(d))
       return MM;
-    Mx[0] = max(Mx[0], abs(d));
-    M[0]  =  - d / Mx[0];
-    M[1]  = max(- Mx[0], min(Mx[0], p1.next(d)));
-    M[1] /= (Mx[1] = max(Mx[1], abs(M[1])));
-    M[2]  = max(- Mx[0], min(Mx[0], p2.next(d)));
-    M[2] /= (Mx[2] = max(Mx[2], abs(M[2])));
+    M[0]  =  - d;
+    M[1]  = p1.next(d);
+    M[2]  = p2.next(d);
     M[3]  = p0.next(d) / var0;
+    M[1] /= (Mx[1] = max(Mx[1], abs(M[1])));
+    M[2] /= (Mx[2] = max(Mx[2], abs(M[2])));
     M[3] /= (Mx[3] = max(Mx[3], abs(M[3])));
     {
       auto qm(makeProgramInvariant<T>(q.next(d)));
@@ -58,22 +57,21 @@ public:
              qqm.first[qqm.first.size() - 2]) / q0[q0.size() - 2] /
            T(int(q0.size())), qqm.second));
     }
-    M[4] /= (Mx[4] = max(Mx[4], abs(M[4])));
     M5   -= d;
+    M[4] /= (Mx[4] = max(Mx[4], abs(M[4])));
     M[5]  = M5 / (Mx[5] = max(Mx[5], abs(M5)));
-    M[6]  = T(int(1)) / T(int(2));
     {
       const auto& ff(f.next(d));
-      Mx[7] = max(abs(d), Mx[7]);
+      Mx[6] = max(abs(d), Mx[6]);
       if(f.full) {
-        M[7] = T(int(0));
-        for(int i = 0; i < ff.size(); i ++) M[7] += ff[i];
-        M[7] = (- M[7]) / (Mx[7] = max(Mx[7], abs(M[7])));
+        M[6] = ff[0];
+        for(int i = 1; i < ff.size(); i ++) M[6] += ff[i];
+        M[6] = (- M[6]) / (Mx[6] = max(Mx[6], abs(M[6])));
       }
     }
     MM    = T(int(0));
     for(int i = 0; i < M.size(); i ++) if(isfinite(M[i])) MM += M[i];
-    return MM *= Mx0 / T(int(8));
+    return MM *= Mx0 / T(int(M.size()));
   }
   P0maxRank<T> p0;
   shrinkMatrix<T, P1I<T, idFeeder<T> > > p1;
@@ -87,28 +85,6 @@ public:
   T var0;
   vector<T> M;
   vector<T> Mx;
-};
-
-// N.B. self similarity.
-template <typename T, typename P> class PF {
-public:
-  inline PF() { ; }
-  inline PF(P&& p, const int& size = 1) {
-    (this->p).resize(size, p);
-    b = idFeeder<T>((this->p).size());
-  }
-  inline ~PF() { ; }
-  inline T next(const T& in) {
-    const auto bb(b.next(in));
-    assert(p.size() == bb.size());
-          auto m(Decompose<T>(p.size()).mother(bb));
-    const auto f(Decompose<T>(p.size()).freq(m, bb));
-    assert(m.size() == p.size());
-    for(int i = 0; i < m.size(); i ++) m[i] = p[i].next(m[i]);
-    return Decompose<T>(p.size()).synth(m, f)[p.size() - 1];
-  }
-  idFeeder<T> b;
-  vector<P> p;
 };
 
 int main(int argc, const char* argv[]) {
@@ -125,13 +101,15 @@ int main(int argc, const char* argv[]) {
   //      () structures. There's differences between them the dimension
   //      original structure should have, however, if the original prediction is
   //      fair enough on counting prediction structure, we can bet only 1 dim.
-  // N.B. also, P0DFT eliminates causes number of the hyper torus whole,
+  // N.B. Also, P0DFT eliminates causes number of the hyper torus whole,
   //      this is not needed in P0maxRank sub class because they treats
   //      better way them, also P1I, P012L because they treats any of the
   //      F_p register (non-)linear calculation.
-  // N.B. however, we need PF for insurance they have a fractal structure cut.
-  // XXX: we need () between initializer and pass args, in clang.
-  PF<num_t, P<num_t> > p((P<num_t>(status)), int(sqrt(num_t(status))));
+  // XXX: However, we need PF for insurance they have a fractal structure cut.
+  //      But this is equivalent to original prediction if original is fair
+  //      enough. The only the reason PF implements is higher frequency part
+  //      and not the highest frequency part (middle of them) complement.
+  P<num_t> p(status);
   num_t d(int(0));
   auto  M(d);
   auto  S(d);
