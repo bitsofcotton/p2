a928f1c9a9a601e26678460d8da613774175f5bf
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2025-02-27 02:05:47 +0900

    add catgp argv1 == 0 condition.

--- catgp.cc
+++ catgp.cc
@@ -30,14 +30,15 @@ int main(int argc, const char* argv[]) {
 #endif
 */
   std::cout << std::setprecision(30);
-  int length(124);
+  int length(0);
   if(argc < 2) std::cerr << argv[0] << " <length>? : continue with ";
   if(1 < argc) length = std::atoi(argv[1]);
-  assert(length);
+  assert(0 <= length);
   std::cerr << argv[0] << " " << length << std::endl;
   // N.B. randtools meaning, v2v tanglement causes up to 3 dimension.
   //      This is also from P1I condition this uses internally.
-  PBond<num_t, P012L<num_t> > p(abs(length), P012L<num_t>());
+  PBond<num_t, P012L<num_t> > p(max(1, length), P012L<num_t>());
+  SimpleVector<num_t> b;
   std::string s;
   num_t d(int(0));
   auto  M(d);
@@ -45,7 +46,12 @@ int main(int argc, const char* argv[]) {
     std::stringstream ins(s);
     ins >> d;
     std::cout << d * M << ", ";
-    std::cout << (M = p.next(d)) << std::endl << std::flush;
+    if(! length) {
+      b.entity.emplace_back(d);
+      if(124 < b.size())
+        M = P012L<num_t>().next(b);
+    } else  M = p.next(d);
+    std::cout << M << std::endl << std::flush;
   }
   return 0;
 }
