b1b12ac09885fd455f0a0070a37d7be652c4a69a
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2025-08-17 15:09:49 +0900

    fix usage bug causes we're lucky.

--- p2.cc
+++ p2.cc
@@ -1148,7 +1148,12 @@ int main(int argc, const char* argv[]) {
   cerr << "# show output statistics it's 0<x<1 (+ for 0<x)" << endl << argv[0] << " T+?" << endl;
   cerr << endl << " *** test case ***" << endl;
   cerr << "cat ... | " << argv[0] << " W | " << argv[0] << " d | " << argv[0] << " d | " << argv[0] << " t " << num_t(int(1)) / num_t(int(4)) << " | tee 0 | " << argv[0] << " Ac | " << argv[0] << " lH > 0+" << endl;
-  cerr << argv[0] << " L 0 0+ | " << argv[0] << " s | " << argv[0] << " O | " << argv[0] << " 0 1 | " << argv[0] << " S ... | " << argv[0] << " k 2 | " << argv[0] << " lH" << endl;
+  cerr << argv[0] << " L 0 0+ | " << argv[0] << " s | " << argv[0] << " O+  > 1-" << endl;
+  cerr << argv[0] << " L 0 0+ | " << argv[0] << " s | " << argv[0] << " s | " << argv[0] << " t " << - num_t(int(1)) << " | " << argv[0] << " O+  > 1+" << endl;
+  cerr << argv[0] << " L 1+ 1- | " << argv[0] << " O+ | " << argv[0] << " t " << num_t(int(1)) / num_t(int(2)) << " > 10" << endl;
+  cerr << argv[0] << " L 0 0+ | " << argv[0] << " s | " << argv[0] << " s | " << argv[0] << " O | " << argv[0] << " 0 ... | " << argv[0] << " S 1 | " << argv[0] << " k 2 | " << argv[0] << " lH" << endl;
+  cerr << argv[0] << " s < 0 | " << argv[0] << " s > 00" << endl;
+  cerr << argv[0] << " L 00 10 | " << argv[0] << " O | " << argv[0] << " 0 ... | " << argv[0] << " k 2 | " << argv[0] << " lH" << endl;
   cerr << endl << " *** graphics test ***" << endl;
   cerr << "yes " << num_t(int(1)) / num_t(int(2)) << " | " << argv[0] << " f ... | head -n 1 | " << argv[0] << " [PY] && mv rand_pgm-0.p[gp]m dummy.p[gp]m" << endl;
   cerr << argv[0] << " P- ... dummy.p[gp]m ... dummy.p[gp]m | tee 0 | <difference-predictor> > 1" << endl; 
